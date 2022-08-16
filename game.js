77c5bc178cc609c90517b0be0517f3b07c8548a8
Christian <c.waldschmidt11@gmail.com>
2022-08-16 15:04:22 -0500

    Update playRound so the result of the game will be output

--- game.js
+++ game.js
@@ -6,13 +6,24 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]
 }   
 
-console.log(getComputerChoice());
-
 const computerSelection = getComputerChoice().toLowerCase();
-const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
-
 console.log(computerSelection)
+const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
 console.log(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win!"
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win!"
+    }
+    else {
+        return "You lose, bitch."
+    }
 }
+
+console.log(playRound(playerSelection, computerSelection));
