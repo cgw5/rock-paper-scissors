00d4ffcc115a5b8fcdd61c6bef31d6fd84a80475
Christian <c.waldschmidt11@gmail.com>
2022-08-17 13:57:00 -0500

    Refine game to show score after each round, play game to 5

--- game.js
+++ game.js
@@ -1,29 +1,55 @@
-console.log("Hello World!!!");
-
+let playerScore = 0
+let computerScore = 0;
 const choices = ["Rock", "Paper", "Scissors"];
 
+// Computer picks either rock, paper, or scissors
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
-}   
+    return choices[Math.floor(Math.random() * choices.length)].toLowerCase()
+}  
 
-const computerSelection = getComputerChoice().toLowerCase();
-console.log(computerSelection)
-const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
-console.log(playerSelection);
+// Player inputs rock, paper, or scissors
+function getPlayerChoice() {
+    return prompt("Choose rock, paper, or scissors").toLowerCase();
+}
 
+// playRound gives result of a round of the game 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a tie"
+        return "It's a tie."
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
         return "You win!"
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
         return "You win!"
     }
     else {
-        return "You lose, bitch."
+        computerScore++
+        return "You lose."
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// for 5 rounds, plays the game
+function game() {
+    while (playerScore < 5 && computerScore < 5) {
+        let computerSelection = getComputerChoice()
+        let playerSelection = getPlayerChoice()
+        playRound(playerSelection, computerSelection)
+        console.log(`Player Score: ${playerScore} `)
+        console.log(`Computer Score: ${computerScore}`)
+        console.log("")
+    }
+    if (playerScore > computerScore) {
+        return "You beat the computer!"
+    }
+    else if (playerScore < computerScore) {
+        return "You lost against the computer."
+    }
+    else {
+        return "It's a draw."
+    }
+}
+
+ console.log(game());
