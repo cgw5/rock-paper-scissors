0edc46c64f836c86ac7ca0c3ece5f16ffbd898bd
Christian <c.waldschmidt11@gmail.com>
2022-08-25 14:55:16 -0500

    Add style, make game play till 5

--- game.js
+++ game.js
@@ -4,11 +4,13 @@ const choices = ["Rock", "Paper", "Scissors"];
 
 let playerScoreCounter = document.getElementById('playerScore')
 let compScoreCounter = document.getElementById('computerScore')
-
+let choice = document.getElementById('choice')
+let result = document.getElementById('result')
 
 const rockButton = document.querySelector('#rockButton');
 const paperButton = document.querySelector('#paperButton');
 const scissorsButton = document.querySelector('#scissorsButton');
+const playAgainButton = document.querySelector('#playAgain');
 
 // clicking on rock calls game function with "rock" as the player choice
 rockButton.addEventListener("click", e =>  {
@@ -25,6 +27,11 @@ scissorsButton.addEventListener("click", e =>  {
     game("scissors");
 });
 
+// clicking "Play Again!" will simply reload the page
+playAgainButton.addEventListener("click", e =>  {
+    location.reload();
+})
+
 // Computer picks either rock, paper, or scissors
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)].toLowerCase()
@@ -35,10 +42,11 @@ function getPlayerChoice() {
     return prompt("Choose rock, paper, or scissors").toLowerCase();
 }
 
-// playRound gives result of a round of the game 
+// playRound gives result of a round of the game and prints result of round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("TIE")
+        result.textContent = "It's a draw.";
         return "It's a tie."
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -49,31 +57,38 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++
         console.log("WIN")
+        result.textContent = "You beat the computer!";
         return "You win!"
     }
     else {
         computerScore++
         console.log("LOSER")
+        result.textContent = "You lost against the computer.";
         return "You lose."
     }
 }
 
 // game function
 function game(playerSelection) {
-    let computerSelection = getComputerChoice()
-    playRound(playerSelection, computerSelection)
-    console.log(`Player Score: ${playerScore} `)
-    console.log(`Computer Score: ${computerScore}`)
-    playerScoreCounter.textContent = `Player Score: ${playerScore} `;
-    compScoreCounter.textContent = `Computer Score: ${computerScore} `;
-    console.log("")
-    if (playerScore > computerScore) {
-        return "You beat the computer!"
-    }
-    else if (playerScore < computerScore) {
-        return "You lost against the computer."
+    if (playerScore == 5 || computerScore == 5) {
     }
     else {
-        return "It's a draw."
+        let computerSelection = getComputerChoice()
+        playRound(playerSelection, computerSelection)
+        playerScoreCounter.textContent = `Player Score: ${playerScore} `;
+        compScoreCounter.textContent = `Computer Score: ${computerScore} `;
+        choice.textContent = `You chose ${playerSelection} and the computer chose ${computerSelection}.`;
+    }
+    if (playerScore == 5) {
+        document.getElementById('rockButton').disabled = true;
+        document.getElementById('paperButton').disabled = true;
+        document.getElementById('scissorsButton').disabled = true;
+        result.textContent = 'You won the game! Congrats!'
+    }
+    else if (computerScore == 5) {
+        document.getElementById('rockButton').disabled = true;
+        document.getElementById('paperButton').disabled = true;
+        document.getElementById('scissorsButton').disabled = true;
+        result.textContent = 'You lost the game. Better luck next time.'
     }
 }
